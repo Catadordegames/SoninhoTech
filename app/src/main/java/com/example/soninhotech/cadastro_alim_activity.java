f9e198bcad6a5f07c9e14b2da5cb6a93f323b214
ikaguia <cristianobrust@sempreceub.com>
2025-05-28 10:50:31 -0300

    Cadastro Alimentação

---
+++ app/src/main/java/com/example/soninhotech/cadastro_alim_activity.java
@@ -0,0 +1,54 @@
+package com.example.soninhotech;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class cadastro_alim_activity extends AppCompatActivity {
+
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_alim_activity);
+
+        ImageButton btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(v -> finish());
+
+        ImageButton btnStartPause = findViewById(R.id.btn_start_pause);
+        ImageButton btnStop = findViewById(R.id.btn_stop);
+        timer = new Timer();
+        timer.outputView = findViewById(R.id.input_time);
+
+        btnStartPause.setOnClickListener(v -> {
+            if (timer.running == 1) {
+                timer.pause();
+                btnStartPause.setImageResource(android.R.drawable.ic_media_play);
+                btnStartPause.setContentDescription(getString(R.string.start));
+            } else {
+                timer.play();
+                btnStartPause.setImageResource(android.R.drawable.ic_media_pause);
+                btnStartPause.setContentDescription(getString(R.string.pause));
+            }
+        });
+
+        btnStop.setOnClickListener(v -> timer.reset());
+
+        Button btnSave = findViewById(R.id.btn_save);
+        btnSave.setOnClickListener(v -> {
+            timer.pause();
+            Handler handler = new Handler();
+            handler.postDelayed(() -> {
+                Snackbar popupMsg = Snackbar.make(v, R.string.report_saved, 4000);
+                popupMsg.show();
+                handler.postDelayed(this::finish, 4000);
+            }, 500);
+        });
+    }
+}
--- app/src/main/java/com/example/soninhotech/tela_principal_activity.java
+++ app/src/main/java/com/example/soninhotech/tela_principal_activity.java
@@ -15,7 +15,13 @@ public class tela_principal_activity extends AppCompatActivity {
         setContentView(R.layout.tela_principal_activity);
 
         Button btnLogout = findViewById(R.id.btn_logout);
-        btnLogout.setOnClickListener(v -> finish());
+        btnLogout.setOnClickListener(v -> {
+            Intent intent = new Intent(
+                tela_principal_activity.this,
+                login_activity.class
+            );
+            startActivity(intent);
+        });
 
         ImageButton btnCadSono = findViewById(R.id.btn_cadastro_sono);
         btnCadSono.setOnClickListener(v -> {
@@ -26,15 +32,14 @@ public class tela_principal_activity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        // TODO: cadastro alimentacao
-//        ImageButton btnCadAlim = findViewById(R.id.btn_cadastro_alim);
-//        btnCadAlim.setOnClickListener(v -> {
-//            Intent intent = new Intent(
-//                tela_principal_activity.this,
-//                cadastro_alimentacao_activity.class
-//            );
-//            startActivity(intent);
-//        });
+        ImageButton btnCadAlim = findViewById(R.id.btn_cadastro_alim);
+        btnCadAlim.setOnClickListener(v -> {
+            Intent intent = new Intent(
+                tela_principal_activity.this,
+                cadastro_alim_activity.class
+            );
+            startActivity(intent);
+        });
 
         Button btnRelSono = findViewById(R.id.btn_relatorio_sono);
         btnRelSono.setOnClickListener(v -> {
